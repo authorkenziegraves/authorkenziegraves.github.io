7149a95a82360508a1c4f9b7ab5bc618eadd0366
KenzieGraves <authorkenziegraves@gmail.com>
2025-04-10 20:13:08 -0500

    Update script.js
    adding script

---
+++ script.js
@@ -0,0 +1,25 @@
+function contactMe(){
+
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        message: document.getElementById("message").value,
+    };
+
+const serviceID = "service_hxl03am";
+const templateID = "template_xi2x9zn";
+
+emailjs.send(serviceID,templateID,params)
+.then(
+    res =>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("phone").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your email has been sent. Thank you!");
+
+    })
+    .catch(err=>console.log(err));
+}
